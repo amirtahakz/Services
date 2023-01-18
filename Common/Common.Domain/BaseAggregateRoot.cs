c0a9154f9541bdad7601689a9160edf4a28bcc8b
amir_taha_kz <amirtaha.zahraei@gmail.com>
2023-01-18 08:52:36 +0330

    Add project files.

---
+++ Common/Common.Domain/BaseAggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Common.Domain
+{
+    public class BaseAggregateRoot : BaseEntity
+    {
+        private readonly List<BaseDomainEvent> _domainEvents = new List<BaseDomainEvent>();
+        [NotMapped]
+        public List<BaseDomainEvent> DomainEvents => _domainEvents;
+        public void AddDomainEvent(BaseDomainEvent eventItem)
+        {
+            _domainEvents.Add(eventItem);
+        }
+        public void RemoveDomainEvent(BaseDomainEvent eventItem)
+        {
+            _domainEvents?.Remove(eventItem);
+        }
+    }
+}
